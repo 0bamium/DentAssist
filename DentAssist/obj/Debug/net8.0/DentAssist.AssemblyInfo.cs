69563e02e3cc3fe1c8802d783a6c9b7cd987ce6e
sebba081 <142901827+sebba081@users.noreply.github.com>
2025-06-16 23:24:03 -0400

    imagenes validaciones

--- DentAssist/obj/Debug/net8.0/DentAssist.AssemblyInfo.cs
+++ DentAssist/obj/Debug/net8.0/DentAssist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentAssist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+100356c5cfeae1a76e8d83faf701881da5aa1ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0440906ab21ab191e5c25bd60e6fa8212cbe990")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentAssist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentAssist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
